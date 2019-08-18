035dfcce4fd387a662dd61e45b138cd86286b4b8
Aman Gupta <sarcasaman@gmail.com>
2019-08-18 18:46:47 +0530

    while and do while loop

---
+++ while_loop.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  int i = 1;
+  while (i <= 10) {
+    if (i % 2 == 0) print(i);
+    i++;
+  }
+
+  //do while loop
+
+  int j = 1;
+
+  print("\n");
+  
+  do {
+    if (j % 2 == 0) print(j);
+    j++;
+  } while (j <= 10);
+}
