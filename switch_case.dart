2b77eb6f09036da346ac26376fa341188e2bb015
Aman Gupta <sarcasaman@gmail.com>
2019-08-18 18:31:26 +0530

    switch case

---
+++ switch_case.dart
@@ -0,0 +1,19 @@
+void main() {
+  String grade = 'A';
+
+//switch - applicable only for int and string
+
+  switch (grade) {
+    case 'A':
+      print("Excellent");
+      break;
+    case 'B':
+      print("Great");
+      break;
+    case 'C':
+      print("Good");
+      break;
+    default:
+      print("Invalid grade");
+  }
+}
