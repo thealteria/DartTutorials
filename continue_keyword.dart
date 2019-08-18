4bd20ed225a7ddbe76120231f34fbfbf8dedc7fa
Aman Gupta <sarcasaman@gmail.com>
2019-08-18 19:11:05 +0530

    continue keyword

---
+++ continue_keyword.dart
@@ -0,0 +1,14 @@
+void main() {
+  for (var i = 0; i <= 10; i++) {
+    if (i % 2 == 0) continue;
+
+    //means if "i" is even then skip the pront statement
+
+    print(i);
+  }
+}
+
+/**
+ * continue - means when the statement is executed then skip all the code after continue
+ *            and continue with the loop
+ */
